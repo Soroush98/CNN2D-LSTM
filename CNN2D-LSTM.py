a93a93ae93c68d36bcf249c8b3f6d7c6424e4ccf
Soroush98 <esmailian98@gmail.com>
2020-09-13 01:01:48 +0430

    Initial commit

---
+++ CNN2D-LSTM.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import os
+import keras
+import keras.backend as K
+import matplotlib.pyplot as plt
+from keras.layers import Dense,GlobalAveragePooling2D
+from keras.applications import MobileNetV2
+from keras.preprocessing import image
+from keras.applications.mobilenet import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers import Conv1D,Flatten,MaxPooling1D,Bidirectional,Reshape,LSTM,Dropout,Embedding,TimeDistributed,MaxPool2D
+from sklearn.metrics import confusion_matrix,plot_confusion_matrix
+
+
+model = Sequential()
+# model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Reshape((1,50,50,3)))
+model.add(TimeDistributed(Convolution2D(32, (3, 3), input_shape=(50, 50, 3), padding='valid', activation= 'relu')) )
+
+model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
+
+model.add(TimeDistributed(Convolution2D(48, (3, 3), padding='valid',activation= 'relu')))
+model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
+
+model.add(TimeDistributed(Convolution2D(64, (3, 3), padding='valid',activation= 'relu')))
+model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
+
+model.add(TimeDistributed(Convolution2D(96, (3, 3), padding='valid',activation= 'relu')))
+model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
+
+model.add(TimeDistributed(Flatten()))
+model.add(Bidirectional(LSTM(200,return_sequences=True)))
+model.add(Dropout(0.25))
+model.add(Bidirectional(LSTM(200,return_sequences=False)))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='linear'))
+model.compile(optimizer='adam', loss='mse')
+train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
+test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+label = pd.read_csv("^GSPC.csv", delimiter=',', names=['id', 'y_val'],header= 0)
+label_t = pd.read_csv("^GSPC_t.csv", delimiter=',', names=['id', 'y_val'],header= 0)
+print(label)
+train_generator=train_datagen.flow_from_dataframe(dataframe = label , directory='Train', x_col="id", y_col="y_val", has_ext=True,
+                                              class_mode="other", target_size=(50, 50 ),
+                                              batch_size=32,shuffle=False)
+test_generator=test_datagen.flow_from_dataframe(dataframe = label_t , directory='Test', x_col="id", y_col="y_val", has_ext=True,
+                                          class_mode="other", target_size=(50, 50 ),
+                                              batch_size=32,shuffle=False)
+
+
+model.compile(optimizer='Adam',loss='mse')
+# Adam optimizer
+# loss function will be categorical cross entropy
+# evaluation metric will be accuracy
+
+step_size_train=train_generator.n//train_generator.batch_size
+step_size_test=test_generator.n//test_generator.batch_size
+model.fit_generator(generator=train_generator,
+                   steps_per_epoch=step_size_train,validation_data=test_generator,validation_steps=step_size_test,
+                   epochs=200)
---
+++ CandleStick_generator.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from mpl_finance import candlestick_ohlc
+
+import matplotlib
+
+matplotlib.use('Agg')  # Bypass the need to install Tkinter GUI framework
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import  numpy as np
+from sklearn import preprocessing
+# Avoid FutureWarning: Pandas will require you to explicitly register matplotlib converters.
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+
+# Load data from CSV file.
+##########################
+my_headers = ['date', 'open', 'high', 'low', 'close','adj close','volume']
+my_dtypes = {'date': 'str', 'open': 'float', 'high': 'float', 'low': 'float',
+             'close': 'float', 'volume': 'int'}
+my_parse_dates = ['date']
+name = '^GSPC_test'
+loaded_data = pd.read_csv('Data/' + name+ '.csv', sep=',', header=1, names=my_headers,
+                          dtype=my_dtypes, parse_dates=my_parse_dates)
+
+# Convert 'Timestamp' to 'float'.
+#   candlestick_ohlc needs time to be in float days format - see date2num().
+loaded_data['date'] = [mdates.date2num(d) for d in loaded_data['date']]
+
+# Re-arrange data so that each row contains values of a day: 'date','open','high','low','close'.
+y_label = []
+quotes = [tuple(x) for x in loaded_data[['date', 'open', 'high', 'low', 'close']].values]
+chunk = 20
+dimension = 50
+df = pd.DataFrame( columns = ['id','y_val'])
+
+print(df)
+for i in range(0,len(quotes) - chunk -1,1):
+    df2 = pd.DataFrame([[str(i) +".png" , quotes[i + chunk][4]] ], columns=['id','y_val'])
+    df = df.append(df2)
+    my_dpi = 96
+    plt.style.use('dark_background')
+    fig = plt.figure(figsize=(dimension / my_dpi,
+                              dimension / my_dpi), dpi=my_dpi)
+    ax1 = fig.add_subplot(1, 1, 1)
+    candlestick_ohlc(ax1, quotes[i:i + chunk], width=1, colorup='#77d879', colordown='#db3f3f', )
+    ax1.grid(False)
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+    ax1.xaxis.set_visible(False)
+    ax1.yaxis.set_visible(False)
+    ax1.axis('off')
+    plt.savefig('Test/' + str(i)  +'.png', pad_inches=0, Transparent='False')
+    plt.close(fig)
+scaler = preprocessing.MinMaxScaler()
+scaled_values = scaler.fit_transform(np.array(df.iloc[:,1]).reshape(-1,1))
+df.iloc[:,1] = scaled_values
+
+df.to_csv("^GSPC_t.csv")
